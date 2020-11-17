dd92c24e737c5ce4b119699e57d1791cb7d53f1c
heyong <henry1778242@foxmail.com>
2020-11-17 23:15:59 +0800

    Bean 的初始化及销毁 Bean 的延迟加载 Bean 的GC流程

--- src/main/java/com/henry/stu/bean/factory/DefaultUserInstanceFactory.java
+++ src/main/java/com/henry/stu/bean/factory/DefaultUserInstanceFactory.java
@@ -1,8 +1,49 @@
 package com.henry.stu.bean.factory;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * @author heyong
  * @date 2020/11/16 19:54
  */
-public class DefaultUserInstanceFactory implements UserInstanceFactory{
+public class DefaultUserInstanceFactory implements UserInstanceFactory, InitializingBean, DisposableBean {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("@PostConstruct 方法初始化...");
+    }
+
+    public void initUser() {
+        System.out.println("@Bean.init-method自定义 方法初始化...");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("实现InitializingBean#afterPropertiesSet() 方法初始化...");
+    }
+
+    @PreDestroy
+    public void distroy() {
+        System.out.println("@PreDestroy 方法销毁...");
+    }
+
+    public void distroyUser() {
+        System.out.println("@Bean.distroy-method自定义 方法销毁...");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("实现DisposableBean#destroy() 方法销毁...");
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("DefaultUserInstanceFactory 正在被回收...");
+        super.finalize();
+    }
 }
