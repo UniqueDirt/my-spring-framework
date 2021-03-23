43feb9fd5642789d7f3e0f09da5f06024786e4dc
heyong <henry1778242@foxmail.com>
2021-03-23 21:05:54 +0800

    不知道提交的啥

--- src/main/java/com/henry/spring/bean/BeanInitializationDemo.java
+++ src/main/java/com/henry/spring/bean/BeanInitializationDemo.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Lazy;
  * Bean 销毁的三种方式  （按执行顺序） 1、@PreDestroy 2、实现DisposableBean#destroy()  3、@Bean(destroy-method) / xml配置
  * 延迟加载方式 1、@Lazy  2、xml 配置 init-lazy   延迟加载的bean是在application context上下文加载完成之后且有被使用时才会加载  一非延迟加载则在容器加载完成前初始化
  * 延迟加载只影响 Bean 的初始化顺序   不影响 Bean 的销毁顺序
+ *
+ * environment 指的是什么
  * @anthor heyong
  * @date
  */
@@ -19,6 +21,8 @@ public class BeanInitializationDemo {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+//        applicationContext.register(BeanInitializationDemo.class, DefaultUserInstanceFactory.class);
 
         applicationContext.register(BeanInitializationDemo.class);
 
